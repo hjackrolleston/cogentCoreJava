eb5763b31cf89cea5ea2f01de25c297c3dc0f17d
hjack <hjack@DESKTOP-KJG9619>
2021-10-14 14:20:10 -0400

    just work for me

--- ProductManager/src/com/cogent/productmanager/exception/IdNotFoundException.java
+++ ProductManager/src/com/cogent/productmanager/exception/IdNotFoundException.java
@@ -3,5 +3,5 @@ import java.lang.Exception;
 import lombok.ToString;
 
 public class IdNotFoundException extends RuntimeException {
-	public IdNotFoundException(String message) {super(message);}    
+	public IdNotFoundException(String message) {super(message);}  
 }
